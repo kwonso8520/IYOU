e18712e8118b76f3384a155b5fe256f7fe1a973a
kwonso8520 <rnjstjrdnjs@dgsw.hs.kr>
2022-12-22 13:04:01 +0900

    IYOU완성
    로비씬추가,4개의 색을 가진 유령오브젝트 추가,Player의 색이 유령의 색과 같다면 점수+2 유령을 Destory 다르다면 점수-1 유령을 랜덤한 위치로 이동

--- Assets/2.script/PlayerControl.cs
+++ Assets/2.script/PlayerControl.cs
@@ -1,13 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
 using UnityEngine;
+using UnityEngine.UIElements;
+using TMPro;
 
 public class PlayerControl : MonoBehaviour
 {
+    public GameObject[] Soul;
+
+    float speed;
+
+    int score;
+    public TextMeshProUGUI tmp;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 5;
+        StartCoroutine("SoulCreate");
     }
 
     // Update is called once per frame
@@ -16,13 +26,70 @@ public class PlayerControl : MonoBehaviour
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
         Vector3 dir = new Vector3(h, v, 0);
-        GetComponent<Rigidbody2D>().velocity = dir * 5;
+        GetComponent<Rigidbody2D>().velocity = dir * speed;
     }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.CompareTag("WALL"))
         {
             GetComponent<SpriteRenderer>().color = collision.collider.GetComponent<SpriteRenderer>().color;
+
+            StopCoroutine("ColorBack");
+            StartCoroutine("ColorBack");
         }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("SOUL")) {
+            //Player와 색이 같다면 = 사라짐
+            if (GetComponent<SpriteRenderer>().color == collision.GetComponent <SpriteRenderer>().color)
+            {
+                speed += 0.5f;
+                score += 2;
+                tmp.text = "score : " + score;
+                Destroy(collision.gameObject);
+                StopCoroutine("SpeedBack");
+                StartCoroutine("SpeedBack");
+            }
+            //Player와 색이 다르다면 = 도망감
+            else if(GetComponent<SpriteRenderer>().color != collision.GetComponent<SpriteRenderer>().color)
+            {
+                float x = Random.Range(-8f, 8f);
+                float y = Random.Range(-3f, 4f);
+                int idx = Random.Range(0, 3 + 1);
+                Vector3 pos = new Vector3(x, y, 0);
+                collision.transform.position = pos;
+
+                speed -= 0.5f;
+                score -= 1;
+                tmp.text = "score : " + score;
+                StopCoroutine("SpeedBack");
+                StartCoroutine("SpeedBack");
+            }
+        }
+    }
+    IEnumerator ColorBack()
+    {
+        yield return new WaitForSeconds(3.5f);
+        GetComponent<SpriteRenderer>().color = Color.white;
+        //Color color = new Color32(255, 255, 255, 255);
+    }
+    IEnumerator SoulCreate()
+    {
+        yield return new WaitForSeconds(3f);
+        //생성코드
+        
+        float x = Random.Range(-8f, 8f);
+        float y = Random.Range(-3f, 4f);
+        int idx = Random.Range(0,3+1);
+        Vector3 pos = new Vector3(x, y, 0);
+
+        Instantiate(Soul[idx],pos,Quaternion.identity);
+        StartCoroutine("SoulCreate");
+    }
+    IEnumerator SpeedBack()
+    {
+        yield return new WaitForSeconds(3f);
+        speed = 5;
     }
 }
